5b8b12c7e438db1f15a072129357b4a2127b0341
MiriamEvardi <miriamevardi97@gmail.com>
2024-05-02 10:34:56 +0200

    added image path

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -22,7 +22,7 @@ export default{
 <template>
     
     <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img :src="'http://localhost:8000/storage/' + project.preview" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{ project.name }}</h5>
             <p class="card-text">Descrizione progetto</p>
