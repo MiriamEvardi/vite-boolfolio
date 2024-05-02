f6bd28de43d86bf580874a3b1c94b8da5339c4f9
MiriamEvardi <miriamevardi97@gmail.com>
2024-05-02 13:09:00 +0200

    removed buttons

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -26,10 +26,8 @@ export default{
         <div class="card-body">
             <h5 class="card-title">{{ project.name }}</h5>
             <p class="card-text">Descrizione progetto</p>
-            <div class="button d-flex">
-                <router-link :to="{name: 'show-project', params: {slug: project.slug}}" class="btn btn-primary">Visualizza</router-link>
-                <a href="#" class="btn btn-warning">Modifica</a>
-                <a href="#" class="btn btn-danger">Elimina</a>
+            <div class="button d-flex justify-content-center">
+                <router-link :to="{name: 'show-project', params: {slug: project.slug}}" class="btn btn-primary ">Visualizza</router-link>
             </div>
         </div>
     </div>
