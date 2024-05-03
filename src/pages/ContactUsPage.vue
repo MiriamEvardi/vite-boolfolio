a12c4bda46af99dcaffd48875d7c6c42abaa14ac
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-05-03 16:33:58 +0200

    ContactUsPage fix

---
+++ src/pages/ContactUsPage.vue
@@ -0,0 +1,22 @@
+<script>
+import ContactForm from '../components/ContactForm.vue';
+
+export default{
+    name: 'ContactUsPage',
+
+    data(){
+        return{
+
+        }
+    }
+}
+
+</script>
+
+<template>
+    <ContactForm></ContactForm>
+</template>
+
+<style lang="scss">
+
+</style>
